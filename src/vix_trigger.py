c7a1294d67a3d8e3bfdd176580aeb8b5459ba664
Uzair Aftab <muaf@nmbu.no>
2021-06-10 19:05:27 +0200

    Migrated code to yfinance

--- src/vix_trigger.py
+++ src/vix_trigger.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-
 import datetime
+import json
 import smtplib
-
+import sys
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-import yfinance as yf
-import requests
-import json
 
+import requests
+import yfinance as yf
+from pandas.core.internals.construction import to_arrays
 
 COMMASPACE = ", "
 
@@ -19,8 +19,8 @@ SYMBOL = "VIX"
 # number of days of historical data to fetch (not counting today)
 days_back = 15
 
-email_from = "williamgreners@gmail.com"
-email_to = ["william.tobias.grenersen@nmbu.no"]
+email_from = "Insert EMAIL HERE"
+email_to = ["INSERT EMAIL here"]
 
 # current datetime
 current_datetime = datetime.datetime.now()
@@ -49,13 +49,15 @@ buy, sell = False, False
 
 
 def isNewHigh(high, data):
+
     """
     Returns True if the 'high' is higher than any of the highs in the data
     array passed in. Otherwise returns False
     """
-    for k, v in data.items():
+    highs = data.get("High")
+    for i in highs:
         try:
-            if (float(v["High"])) >= float(high):
+            if (float(i)) >= float(high):
                 return False
         except ValueError:
             return False
@@ -67,9 +69,11 @@ def isNewLow(low, data):
     Returns True if the 'low' is lower than any of the lows in the data
     array passed in. Otherwise returns False
     """
-    for k, v in data.items():
+    lows = data.get("Low")
+    print(low)
+    for i in lows:
         try:
-            if (float(v["Low"])) <= float(low):
+            if float(i) <= float(low):
                 return False
         except ValueError:
             return False
@@ -133,34 +137,31 @@ end = current_datetime - datetime.timedelta(days=1)
 # 30 days ago from today
 start = current_datetime - datetime.timedelta(days=30)
 
-# fetch the historical data from yahoo
-# data = ystockquote.get_historical_prices('^' + SYMBOL, start.strftime("%Y-%m-%d"), end.strftime("%Y-%m-%d"))
-# all_data = ystockquote.get_all('^' + SYMBOL)
-
 vix = yf.Ticker(f"^{SYMBOL}")
 data = vix.history(start=start.strftime("%Y-%m-%d"), end=end.strftime("%Y-%m-%d"))
-print(data.size)
-data = data.to_dict()
+data.drop(labels=["Volume", "Dividends", "Stock Splits"], axis=1, inplace=True)
+index = data.columns
+data = data.reset_index().drop("Date", axis=1)
+data = data.to_dict("list")
 print(data)
-print(len(data))
-
 # parsed_json = json.loads(data)
+
 
 all_data = vix.history(period="1mo", interval="1d")
 # validate that we have enough historical data to continue
+
 if len(data) < days_back:
     print(
         "Not enough historical data available to continue (need at least 15 days of market data)"
     )
-    #sys.exit(1)
+    # sys.exit(1)
 
 
 current = vix.info
 current = current.get("regularMarketPrice")
-print(current)
 
-open = all_data["Open"]
-open_list = open.to_list()
+opens = all_data["Open"]
+open_list = opens.to_list()
 newest_open = open_list[-1]
 
 high = all_data["High"]
@@ -174,20 +175,18 @@ newest_low = low[-1]
 
 # if the current price is higher than the open and today is a new 15-day low,
 # then this is a SELL indicator
-if isCurrentHigherThanOpen(current, newest_open) & isNewLow(low, data):
+if isCurrentHigherThanOpen(current, newest_open) & isNewLow(newest_low, data):
     # print 'today is a 15-day low (' + str(low) + ') & the current price (' + str(current) + ') is higher than the open (' + str(open) + ')'
     sell = True
 
 # if the current price is lower than the open and today is a new 15-day high,
 # then this is a BUY indicator
-if isCurrentLowerThanOpen(current, open) & isNewHigh(high, data):
+if isCurrentLowerThanOpen(current, newest_open) & isNewHigh(newest_high, data):
     # print 'today is a 15-day high (' + str(high) + ') & the current price (' + str(current) + ') is lower than the open (' + str(open) + ')'
     buy = True
-
 # For testing
 # buy = True
 # current_datetime = (current_datetime - datetime.timedelta(hours=3))
-
 if buy | sell:
     # reversal indicator is true.
     if buy:
@@ -279,24 +278,25 @@ file.close()
 chart_image = r.content
 sendEmail()
 
-"""
-print 'start date is: ' + str(start)
-print 'end date is: ' + str(end)
-print 'current time is: ' + str(current_datetime)
-print 'current hour is: ' + str(current_datetime.hour)
-print 'current minute is: ' + str(current_datetime.minute)
-print 'current second is: ' + str(current_datetime.second)
-print 'number of days of actual market data retrieved is: ' + str((len(data) - 1))
-print 'Current price: ' + str(current)
-print 'Open price: ' + str(open)
-print 'High price: ' + str(high)
-print 'Low price: ' + str(low)
+
+def print_data_summary():
+    print("start date is: " + str(start))
+    print("end date is: " + str(end))
+    print("current time is: " + str(current_datetime))
+    print("current hour is: " + str(current_datetime.hour))
+    print("current minute is: " + str(current_datetime.minute))
+    print("current second is: " + str(current_datetime.second))
+    print("number of days of actual market data retrieved is: " + str((len(data) - 1)))
+    print("Current price: " + str(current))
+    print("Open price: " + str(open))
+    print("High price: " + str(high))
+    print("Low price: " + str(low))
+
 
 # Loop through the previous 14-days of data (the first item in the data array
 # is omitted because it is just header information
-print 'the previous 14-day of historical data:'
-for i in range(1, days_back):
-    print data[i][0] + ': High:' + data[i][2] + ' Low:' + data[i][3]
-print subject
-print body
-"""
+# print('the previous 14-day of historical data:')
+# for i in range(1, days_back):
+#    print(data[i][0] + ': High:' + data[i][2] + ' Low:' + data[i][3])
+print(subject)
+print(body)
